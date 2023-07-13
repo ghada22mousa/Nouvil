ec812553fc709971e6084891da52f422560e1ddc
ghada22mousa <129544672+ghada22mousa@users.noreply.github.com>
2023-07-13 17:51:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+window.onscroll = function(){
+    let value = scrollY;
+    stars.style.left = value +'px';
+    moon.style.top = value * 4 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value + 'px';
+    boat.style.top = value + 'px';
+    boat.style.left = value * 3 + 'px';
+    nouvil.style.fontSize = value + 'px';
+    if (scrollY >= 67){
+        nouvil.style.fontSize = 67 + 'px';
+        nouvil.style.position = 'fixed';
+        if (scrollY >= 478){
+            nouvil.style.display = 'none';
+        }
+    else{
+        nouvil.style.display = 'block';
+    }
+    if (scrollY >= 113){
+       document.querySelector('.main').style.background = 'linear-gradient(#5c7e9b,#1f081d)';
+    }
+    else{
+       document.querySelector('.main').style.background = 'linear-gradient(#350518, #131112)'
+    }
+  }
+}
